2bffb75bde248a87b4f38ff4565d66d4cce000c7
allen <allen000728@gmail.com>
2023-03-10 17:33:22 +0800

    Initial Commit

---
+++ Cocktail_SwiftUI/Cocktail_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cocktail_SwiftUIApp.swift
+//  Cocktail_SwiftUI
+//
+//  Created by allen on 2023/3/10.
+//
+
+import SwiftUI
+
+@main
+struct Cocktail_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
